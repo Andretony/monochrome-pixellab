505944cd675c73146378d2641397203bdd24836c
Andretony <75176677+Andretony@users.noreply.github.com>
2022-05-23 15:44:17 +0300

    344

--- components/header.jsx
+++ components/header.jsx
@@ -64,76 +64,96 @@ export default function Header() {
             <span>Monochrome</span>
           </div>
 
-          <section className="header-controls">
-            <div className="header-controls">
-              <ul>
-                <li>
-                  <form className="header-search d-none d-lg-block">
-                    <input
-                      type="text"
-                      name="Search"
-                      id="Search"
-                      placeholder="Search"
-                    ></input>
-                    <button title="Search">
-                      <i className="fa-solid fa-magnifying-glass fa-xl"></i>
-                    </button>
-                  </form>
-                </li>
-                <li>
-                  <a href="" title="user">
-                    <i className="fa-regular fa-user"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="" title="wishlist">
-                    <i className="fa-regular fa-heart"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="" title="Shopping-bag">
-                    <i className="fa-solid fa-bag-shopping"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div className="header-controls">
+            <form className="header-search  d-lg-flex flex-row-reverse d-none">
+              <input
+                type="text"
+                name="Search"
+                id="Search"
+                placeholder="Search"
+              />
+              <button title="Search">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </form>
+
+            <ul>
+              <li>
+                <a href="" title="user">
+                  <i className="fa-regular fa-user"></i>
+                </a>
+              </li>
+              <li>
+                <a href="" title="wishlist">
+                  <i className="fa-regular fa-heart"></i>
+                </a>
+              </li>
+              <li>
+                <a href="" title="Shopping-bag">
+                  <i className="fa-solid fa-bag-shopping"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section className="header-bottom">
+          <nav className="nav-main d-none d-lg-block">
+            <ul>
+              <li>
+                <a href="" title="New arrivals">
+                  New Arrivals
+                </a>
+              </li>
+              <li>
+                <a href="" title="Brands">
+                  Brands
+                </a>
+              </li>
+              <li>
+                <a href="" title="Men's">
+                  Men's
+                </a>
+              </li>
+              <li>
+                <a href="" title="Women's">
+                  Women's
+                </a>
+              </li>
+              <li>
+                <a href="" title="Accessories">
+                  Accessories
+                </a>
+              </li>
+              <li>
+                <a href="" title="Lookbook"></a>
+              </li>
+            </ul>
+          </nav>
+          <section className="header-primary d-lg-none ">
+            <ul className="d-flex flex-row">
+              <li>
+                <a href="" title="Menu">
+                  <i className="fa-solid fa-bars"></i>
+                </a>
+              </li>
+
+              <li className="d-flex flex-row">
+                <form>
+                  <input
+                    type="text"
+                    name="Search"
+                    id="Search"
+                    placeholder="Search"
+                  />
+                  <button title="Search">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                  </button>
+                </form>
+              </li>
+            </ul>
           </section>
         </section>
-      </section>
-
-      <section className="header-bottom">
-        <nav className="nav-main d-none d-lg-block">
-          <ul>
-            <li>
-              <a href="" title="New arrivals">
-                New Arrivals
-              </a>
-            </li>
-            <li>
-              <a href="" title="Brands">
-                Brands
-              </a>
-            </li>
-            <li>
-              <a href="" title="Men's">
-                Men's
-              </a>
-            </li>
-            <li>
-              <a href="" title="Women's">
-                Women's
-              </a>
-            </li>
-            <li>
-              <a href="" title="Accessories">
-                Accessories
-              </a>
-            </li>
-            <li>
-              <a href="" title="Lookbook"></a>
-            </li>
-          </ul>
-        </nav>
       </section>
     </header>
   );
